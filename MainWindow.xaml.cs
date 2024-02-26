45cc5f933db4534a9b14557c9175c7238719ea7d
dinara-khusainova <xusainova.dinara@bk.ru>
2024-02-26 23:31:55 +0300

    add

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿
+using SoftTradePlusAPP.ViewModel;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace SoftTradePlusAPP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static ListView AllManagersView;
+        public static ListView AllClientsView;
+        public static ListView AllProductsView;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new DataManagerVM();
+
+            AllManagersView = ViewAllManagers;
+            AllClientsView = ViewAllClients;
+            AllProductsView = ViewAllProducts;
+        }
+       
+    }
+}
